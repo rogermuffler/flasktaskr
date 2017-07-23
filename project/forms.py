741a4c883576bde937a500060ab4f4f0a0d2e38d
rogermuffler <roger@stammtischphilosoph.ch>
2017-07-23 12:43:49 +0200

    finish page 349

--- project/forms.py
+++ project/forms.py
@@ -7,6 +7,6 @@ from wtforms.validators import DataRequired
 class AddTaskForm(Form):
     task_id = IntegerField()
     name = StringField('Task Name', validators=[DataRequired()])
-    due_date = DateField('Date Due (mm/dd/yyyy)', validators=[DataRequired()], format='%m/%d/%Y')
+    due_date = DateField('Date Due (yyyy-mm-dd)', validators=[DataRequired()], format='%Y-%m-%d')
     priority = SelectField('Priority', validators=[DataRequired()], choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
     status = IntegerField('Status')
